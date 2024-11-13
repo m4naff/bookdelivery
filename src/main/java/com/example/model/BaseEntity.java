2696124dba02e1577dedd5a93c82deae5683867f
themanaff <ahmedovmenaf@gmail.com>
2024-11-13 16:58:00 +0400

    Firs model BaseEntity created. Next operation is to create CustomUserDetails

---
+++ src/main/java/com/example/model/BaseEntity.java
@@ -0,0 +1,55 @@
+package com.example.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.time.LocalDateTime;
+
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Column(name = "created_user")
+    protected String createdUser;
+
+    @Column(name = "created_at")
+    protected LocalDateTime createdAt;
+
+    @Column(name = "updated_user")
+    protected String updatedUser;
+
+    @Column(name = "updated_at")
+    protected LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdUser = getUsernameFromAuthentication();
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedUser = getUsernameFromAuthentication();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    private String getUsernameFromAuthentication() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof CustomUserDetails userDetails) {
+            return userDetails.getUsername();
+        }
+        return "anonymousUser";
+    }
+
+}
