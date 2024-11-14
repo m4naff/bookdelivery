3c794373fd111004e894a013b6879262d9f220af
themanaff <ahmedovmenaf@gmail.com>
2024-11-14 21:27:41 +0400

    CustomUserDetailsService is ready

---
+++ src/main/java/com/example/security/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.example.security;
+
+import com.example.model.User;
+import com.example.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Custom implementation of Spring Security's UserDetailsService interface
+ * for loading user details by username during authentication.
+ */
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = null;
+        try {
+            user = userService.findByUsername(username)
+                    .orElseThrow(() -> new Exception("User Name " + username + " not found"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return new CustomUserDetails(user);
+    }
+}
