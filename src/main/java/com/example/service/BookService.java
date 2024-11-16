eb81709341600683af0b2fe8d35cf31b3c8c63a8
themanaff <ahmedovmenaf@gmail.com>
2024-11-16 12:27:35 +0400

    BookUpdateStockRequest is ready

--- src/main/java/com/example/service/BookService.java
+++ src/main/java/com/example/service/BookService.java
@@ -2,6 +2,7 @@ package com.example.service;
 
 import com.example.demo.BookDTO;
 import com.example.payload.request.book.BookCreateRequest;
+import com.example.payload.request.book.BookUpdateStockRequest;
 
 /**
  * This interface defines a service for managing books.
@@ -15,5 +16,22 @@ public interface BookService {
      * @return A {@link BookDTO} representing the newly created book.
      */
     BookDTO createBook(BookCreateRequest request);
+
+    /**
+     * Retrieves a book by its unique identifier.
+     *
+     * @param bookId The unique identifier of the book.
+     * @return A {@link BookDTO} representing the requested book.
+     */
+    BookDTO getBookById(String bookId);
+
+    /**
+     * Updates the stock quantity of a book by its unique identifier.
+     *
+     * @param bookId The unique identifier of the book.
+     * @param request The request containing the updated stock information.
+     * @return A {@link BookDTO} representing the book after the stock update.
+     */
+    BookDTO updateBookStockById(String bookId, BookUpdateStockRequest request);
 
 }
