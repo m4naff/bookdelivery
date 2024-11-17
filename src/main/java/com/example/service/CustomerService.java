30818a6f73e0169f7e68d11cd94cb9a31085ea41
themanaff <ahmedovmenaf@gmail.com>
2024-11-17 23:15:55 +0400

    CustomerCreateRequest created for creating a customer

---
+++ src/main/java/com/example/service/CustomerService.java
@@ -0,0 +1,18 @@
+package com.example.service;
+
+import com.example.payload.request.customer.CustomerCreateRequest;
+
+/**
+ * This interface defines a service for managing customer-related operations.
+ */
+public interface CustomerService {
+
+    /**
+     * Creates a new customer based on the provided customer creation request.
+     *
+     * @param customerCreateRequest The request containing customer information to be used for creation.
+     * @return A {@link UserDTO} representing the newly created customer.
+     */
+    UserDTO createCustomer(CustomerCreateRequest customerCreateRequest);
+
+}
