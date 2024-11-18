32d730c99e7a90794524b95f19f7658e3b146734
themanaff <ahmedovmenaf@gmail.com>
2024-11-19 00:19:01 +0400

    OrderService implementation has finished

--- src/main/java/com/example/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/service/impl/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.exception.order.OrderNotFoundException;
 import com.example.model.enums.Role;
 import com.example.model.mapper.order.OrderMapper;
 import com.example.payload.request.book.PaginationRequest;
+import com.example.payload.request.pagination.DateIntervalRequest;
 import com.example.payload.request.pagination.PaginatedFindAllRequest;
 import com.example.repository.OrderRepository;
 import com.example.security.CustomUserDetails;
@@ -50,13 +51,40 @@ public class OrderServiceImpl implements OrderService {
                 .orElseThrow(() -> new OrderNotFoundException("Order not found with id: " + id));
     }
 
+    /**
+     * Retrieves a paginated list of all orders associated with a customer based on their unique identifier.
+     *
+     * @param customerId The unique identifier of the customer.
+     * @param paginationRequest The request containing pagination information.
+     * @return A {@link Page} of {@link OrderDTO} objects representing the list of orders for the customer.
+     */
     @Override
     public Page<OrderDTO> findAllOrdersByCustomerId(Long customerId, PaginationRequest paginationRequest) {
-        return null;
+        final CustomUserDetails userDetails = identify.getCustomUserDetails();
+        final Role userRole = userDetails.getUser().getRole();
+        if(userRole.equals(Role.ROLE_CUSTOMER) && userDetails.getId().equals(customerId)
+            || userRole.equals(Role.ROLE_ADMIN)) {
+            return orderRepository.findAllByUserId(customerId, paginationRequest.toPageable())
+                    .map(OrderMapper::toOrderDTO);
+        }
+        throw new AccessDeniedException("You cannot access this order by Id");
     }
 
+    /**
+     * Retrieves a paginated list of all orders within a specified date interval.
+     *
+     * @param paginatedFindAllRequest The request containing date interval and pagination information.
+     * @return A {@link Page} of {@link OrderDTO} objects representing the list of orders within the specified date interval.
+     */
     @Override
     public Page<OrderDTO> findAllOrdersBetweenTwoDatesAndPagination(PaginatedFindAllRequest paginatedFindAllRequest) {
-        return null;
+        DateIntervalRequest dateIntervalRequest = paginatedFindAllRequest.getDateIntervalRequest();
+        PaginationRequest paginationRequest = paginatedFindAllRequest.getPaginationRequest();
+
+        return orderRepository.findAllByCreatedAtBetween(
+                dateIntervalRequest.getStartDate(),
+                dateIntervalRequest.getEndDate(),
+                paginationRequest.toPageable()
+        ).map(OrderMapper::toOrderDTO);
     }
 }
