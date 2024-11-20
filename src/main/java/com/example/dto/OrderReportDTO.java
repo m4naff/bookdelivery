13703f805368ae02499e8395c33abb77545f1e9c
themanaff <ahmedovmenaf@gmail.com>
2024-11-20 17:10:43 +0400

    findOrderStatisticsByCustomerId query method added to OrderRepository interface

---
+++ src/main/java/com/example/dto/OrderReportDTO.java
@@ -0,0 +1,25 @@
+package com.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+/**
+ * Data Transfer Object (DTO) representing order report information.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderReportDTO {
+
+    private String month;
+    private Integer year;
+    private Long totalOrderCount;
+    private Long totalBookCount;
+    private BigDecimal totalPrice;
+
+}
--- src/main/java/com/example/repository/OrderRepository.java
+++ src/main/java/com/example/repository/OrderRepository.java
@@ -1,10 +1,13 @@
 package com.example.repository;
 
+import com.example.dto.OrderReportDTO;
 import com.example.model.Order;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 
@@ -33,5 +36,30 @@ public interface OrderRepository extends JpaRepository<Order, Long>, JpaSpecific
      * @return A Page containing the orders created within the specified time range.
      */
     Page<Order> findAllByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endTime, Pageable orderPageable);
+
+    /**
+     * <p>Retrieves a paginated list of order statistics for a specific customer.</p>
+     * <p>
+     * This method retrieves a paginated list of order statistics, including the following items:
+     * <p>- Total count of orders</p>
+     * <p>- Total count of unique books</p>
+     * <p>- Total price</p>
+     *
+     * <p>The statistics are grouped by the month and year of order creation date, and are ordered by
+     * year in descending order.</p>
+     *
+     * @param customerId The ID of the customer for whom the order statistics are to be retrieved.
+     * @param pageable   The pagination information.
+     * @return A {@link Page} of {@link OrderReportDTO} objects containing the order statistics.
+     */
+    @Query("""
+        select new com.example.dto.OrderReportDTO
+        (function('monthname',o.createdAt),function('year',o.createdAt),count(o.id),count(b.id),sum(b.price))
+        from Order o inner join o.orderItems items inner join items.book b
+        where (o.user.id = :customerId)
+        group by function('monthname',o.createdAt),function('year',o.createdAt)
+        order by function('year', o.createdAt) desc 
+    """)
+    Page<OrderReportDTO> findOrderStatisticsByCustomerId(@Param("customerId") Long customerId, Pageable pageable);
 
 }
