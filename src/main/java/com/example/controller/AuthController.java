4750cc79ed6194aa89dbaedc4c0ed0bcfbfa81b5
themanaff <ahmedovmenaf@gmail.com>
2024-11-23 14:34:14 +0400

    AuthController and CustomResponse classes created. CustomResponse has finished

---
+++ src/main/java/com/example/controller/AuthController.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import com.example.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+
+
+}
