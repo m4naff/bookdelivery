526b045fb677906095318ac02888abe5d3e9da3c
themanaff <ahmedovmenaf@gmail.com>
2024-11-23 16:25:26 +0400

    springdoc-openapi-ui dependency added for @SecurityRequirement annotation in BookController

---
+++ src/main/java/com/example/controller/BookController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/books")
+@RequiredArgsConstructor
+@SecurityRequirement(name = "bearerAuth")
+public class BookController {
+
+}
