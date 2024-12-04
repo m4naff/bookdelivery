3bb902010f84529b116714c60ab6c9b533c89d34
themanaff <ahmedovmenaf@gmail.com>
2024-12-05 03:28:04 +0400

    getOrdersByCustomerId method created and some mappings added into it

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -2,14 +2,18 @@ package com.example.controller;
 
 import com.example.dto.OrderDTO;
 import com.example.model.mapper.order.OrderMapper;
+import com.example.payload.request.book.PaginationRequest;
 import com.example.payload.request.order.CreateOrderRequest;
+import com.example.payload.response.CustomPageResponse;
 import com.example.payload.response.auth.CustomResponse;
 import com.example.payload.response.order.OrderCreatedResponse;
+import com.example.payload.response.order.OrderGetByCustomerResponse;
 import com.example.payload.response.order.OrderGetResponse;
 import com.example.service.OrderSaveService;
 import com.example.service.OrderService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -39,6 +43,22 @@ public class OrderController {
         final OrderDTO orderDto = orderService.findOrderById(orderId);
         final OrderGetResponse response = OrderMapper.toGetResponse(orderDto);
         return CustomResponse.ok(response);
+    }
+
+    @GetMapping("/customer/{customerId}")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_CUSTOMER')")
+    public CustomResponse<CustomPageResponse<OrderGetByCustomerResponse>> getOrdersByCustomerId(
+            @PathVariable Long customerId,
+            @RequestBody PaginationRequest paginationRequest
+            ) {
+
+        final Page<OrderDTO> pageOfOrderDTOs = orderService.findAllOrdersByCustomerId(customerId, paginationRequest);
+
+        final CustomPageResponse<OrderGetByCustomerResponse> response = OrderMapper
+                .toGetByCustomerResponse(pageOfOrderDTOs);
+
+        return CustomResponse.ok(response);
+
     }
 
 }
--- src/main/java/com/example/model/mapper/order/OrderMapper.java
+++ src/main/java/com/example/model/mapper/order/OrderMapper.java
@@ -3,9 +3,12 @@ package com.example.model.mapper.order;
 import com.example.dto.OrderDTO;
 import com.example.model.Order;
 import com.example.model.mapper.user.UserMapper;
+import com.example.payload.response.CustomPageResponse;
 import com.example.payload.response.order.OrderCreatedResponse;
+import com.example.payload.response.order.OrderGetByCustomerResponse;
 import com.example.payload.response.order.OrderGetResponse;
 import lombok.experimental.UtilityClass;
+import org.springframework.data.domain.Page;
 
 /**
  * Utility class for mapping operations related to {@link Order} and {@link OrderDTO}.
@@ -58,6 +61,28 @@ public class OrderMapper {
                 .build();
     }
 
+    /**
+     * Converts an {@link OrderDTO} object to an {@link OrderGetByCustomerResponse}.
+     *
+     * @param source The {@link OrderDTO} object to be converted.
+     * @return An {@link OrderGetByCustomerResponse} containing data from the source DTO.
+     */
+    public static OrderGetByCustomerResponse toGetByCustomerResponse(OrderDTO source) {
+        return OrderGetByCustomerResponse.builder()
+                .id(source.getId())
+                .user(source.getUser())
+                .orderItems(source.getOrderItems())
+                .createdAt(source.getCreatedAt())
+                .build();
+    }
 
-
+    /**
+     * Converts a {@link Page<OrderDTO>} to a {@link CustomPageResponse<OrderGetByCustomerResponse>}.
+     *
+     * @param sources The source {@link Page<OrderDTO>} to be converted.
+     * @return A {@link CustomPageResponse<OrderGetByCustomerResponse>} containing converted data.
+     */
+    public static CustomPageResponse<OrderGetByCustomerResponse> toGetByCustomerResponse(Page<OrderDTO> sources) {
+        return CustomPageResponse.of(sources.map(OrderMapper::toGetByCustomerResponse));
+    }
 }
